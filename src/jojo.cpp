a9ebd8ef606b8d26e10c316fc0a1e0b04bb8db26
xieyuheng <xyheme@gmail.com>
2018-06-29 17:47:25 +0800

    up

--- src/jojo.cpp
+++ src/jojo.cpp
@@ -2,23 +2,64 @@
 
 using namespace std;
 
-struct env_s {
-    string name_map;
-    string obj_stack;
-    string frame_stack;
+using name_t = string;
+
+struct env_s
+{
+    map<name_t, obj_s> name_map;
+    name_t obj_stack;
+    name_t frame_stack;
     void step();
     void eval();
 };
 
-struct den_s {
+struct obj_s
+{
 
 };
 
-struct obj_s {
+struct clo_obj_s: public obj_s
+{
 
 };
 
-struct ins_s {
+struct int_obj_s: public obj_s
+{
+
+};
+
+struct str_obj_s: public obj_s
+{
+
+};
+
+struct ins_s
+{
+
+};
+
+struct call_ins_s: public ins_s
+{
+
+};
+
+struct end_ins_s: public ins_s
+{
+
+};
+
+struct get_ins_s: public ins_s
+{
+
+};
+
+struct let_ins_s: public ins_s
+{
+
+};
+
+struct clo_ins_s: public ins_s
+{
 
 };
 
