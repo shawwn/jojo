1d08ae17d6e13c4a4e6023130356ad1e6767ddca
xieyuheng <xyheme@gmail.com>
2018-08-03 17:21:55 +0800

    fix data.hpp

--- src/object/data.hpp
+++ src/object/data.hpp
@@ -10,35 +10,3 @@ struct data_o: obj_t
     bool equal (env_t &env, shared_ptr <obj_t> obj);
     string repr (env_t &env);
 };
-
-bool
-data_o::equal (env_t &env, shared_ptr <obj_t> obj)
-{
-    if (this->tag != obj->tag) return false;
-    auto that = static_pointer_cast <data_o> (obj);
-    return obj_map_equal (env, this->obj_map, that->obj_map);
-
-}
-
-string
-data_o::repr (env_t &env)
-{
-    if (this->obj_map.size () == 0) {
-        string repr = "";
-        repr += this->tag;
-        repr.pop_back ();
-        repr.pop_back ();
-        repr += "-c";
-        return repr;
-    }
-    else {
-        string repr = "(";
-        repr += this->tag;
-        repr.pop_back ();
-        repr.pop_back ();
-        repr += "-c ";
-        repr += obj_map_repr (env, this->obj_map);
-        repr += ")";
-        return repr;
-    }
-}
