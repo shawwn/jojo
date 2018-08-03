f6285cc0a374229ace6aa5af71e81423b02ce829
xieyuheng <xyheme@gmail.com>
2018-08-03 18:15:14 +0800

    be able to build now

--- src/libjojo.hpp
+++ src/libjojo.hpp
@@ -39,9 +39,103 @@ using string_vector_t = vector <string>;
 using local_ref_t = pair <size_t, size_t>;
 using local_ref_map_t = map <name_t, local_ref_t>;
 
-#include "env.hpp"
-#include "obj.hpp"
-#include "jo.hpp"
+// #include "env.hpp"
+
+struct env_t
+{
+    box_map_t box_map;
+    obj_stack_t obj_stack;
+    frame_stack_t frame_stack;
+    void step ();
+    void run ();
+    void box_map_report ();
+    void frame_stack_report ();
+    void obj_stack_report ();
+    void report ();
+    void run_with_base (size_t base);
+    void double_report ();
+    void step_and_report ();
+};
+
+struct jojo_t
+{
+    jo_vector_t jo_vector;
+    jojo_t (jo_vector_t jo_vector);
+    ~jojo_t ();
+};
+
+struct frame_t
+{
+    size_t index;
+    shared_ptr <jojo_t> jojo;
+    local_scope_t local_scope;
+    frame_t (shared_ptr <jojo_t> jojo, local_scope_t local_scope);
+};
+
+struct box_t
+{
+    shared_ptr <obj_t> obj;
+    bool empty_p;
+    box_t ();
+    box_t (shared_ptr <obj_t> obj);
+};
+
+shared_ptr <jojo_t>
+jojo_append (shared_ptr <jojo_t> ante,
+             shared_ptr <jojo_t> succ);
+
+void
+jojo_print (env_t &env, shared_ptr <jojo_t> jojo);
+
+void
+jojo_print_with_index (env_t &env,
+                       shared_ptr <jojo_t> jojo,
+                       size_t index);
+
+void
+frame_report (env_t &env, shared_ptr <frame_t> frame);
+
+box_t *
+boxing (env_t &env, name_t name);
+
+name_t
+name_of_box (env_t &env, box_t *box);
+
+// #include "obj.hpp"
+
+struct obj_t
+{
+    tag_t tag;
+    obj_map_t obj_map;
+    virtual ~obj_t ();
+    virtual string repr (env_t &env);
+    virtual bool equal (env_t &env, shared_ptr <obj_t> obj);
+    virtual void apply (env_t &env, size_t arity);
+};
+
+using sig_t = name_vector_t;
+
+void
+define (env_t &env,
+        name_t name,
+        shared_ptr <obj_t> obj);
+
+name_t
+name_of_sig (sig_t &sig);
+
+name_vector_t
+name_vector_of_sig (sig_t &sig);
+
+// #include "jo.hpp"
+
+struct jo_t
+{
+    virtual jo_t * copy ();
+    virtual ~jo_t ();
+    virtual void exe (env_t &env, local_scope_t &local_scope);
+    virtual string repr (env_t &env);
+};
+
 
 template <typename Out>
 void
--- src/syntax/local_ref_jo.cpp
+++ src/syntax/local_ref_jo.cpp
@@ -1,5 +1,7 @@
 #include "local_ref_jo.hpp"
 
+#include "../libjojo.hpp"
+
 local_ref_jo_t::
 local_ref_jo_t (size_t level, size_t index)
 {
@@ -19,9 +21,12 @@ local_ref_jo_t::exe (env_t &env, local_scope_t &local_scope)
     // this is the only place where
     //   the local_scope in the arg of exe is uesd.
     auto bind_vector =
-        vector_rev_ref (local_scope, this->level);
+        // vector_rev_ref (local_scope, this->level);
+        local_scope [local_scope.size () - this->level - 1];
     auto bind =
-        vector_rev_ref (bind_vector, this->index);
+        // vector_rev_ref (bind_vector, this->index);
+        bind_vector [local_scope.size () - this->index - 1];
+
     auto obj = bind.second;
     env.obj_stack.push (obj);
 }
