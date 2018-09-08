6adcb2b00a8dda3949f9f7983d70f8b293c90933
Xie Yuheng <xyheme@gmail.com>
2018-09-09 06:22:22 +0800

    - fix test_jojo.rs

--- tests/test_jojo.rs
+++ tests/test_jojo.rs
@@ -1,12 +1,30 @@
 extern crate jojo;
 
+use std::fs;
+use std::io;
+use std::fs::DirEntry;
 use std::path::Path;
 
 #[test]
-fn test_jojo () {
-    let module_path = Path::new ("tests/jojo/test-simple.jo");
-    let _env = jojo::Env::from_module_path (module_path);
+fn test_jojo () -> io::Result <()> {
+    visit_dirs (Path::new ("tests/jojo"), &|entry| {
+        let module_path = entry.path ();
+        let _env = jojo::Env::from_module_path (&module_path);
+    })
+}
 
-    let module_path = Path::new ("tests/jojo/test-str.jo");
-    let _env = jojo::Env::from_module_path (module_path);
+fn visit_dirs (
+    dir: &Path,
+    cb: &Fn (&DirEntry),
+) -> io::Result <()> {
+    if dir.is_dir () {
+        for entry in fs::read_dir (dir)? {
+            let entry = entry?;
+            let path = entry.path ();
+            if path.is_file () {
+                cb (&entry);
+            }
+        }
+    }
+    Ok (())
 }
